038f5d19d3b00f3e0a0b2e8cb7bd3395b8749a8b
Khushi Singh <khushisingh48744@gmail.com>
2024-11-04 14:22:50 +0530

    second commit

--- add.cpp
+++ add.cpp
@@ -5,6 +5,8 @@ int main()
     int a=10;
     int b=20;
     int c=a+b;
+    int d=a-b;
     cout<<"the addition of two  number is "<<c;
+    cout<<"the subtraction of two numbers is"<<d;
     return 0;
 }
